b027b8a7ad13e6ef37a865477a987981aa8ec114
Aditya <kadamaditya1973@gmail.com>
2024-01-13 20:24:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import './App.css'
 import Page from './component/page';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Page />
